2b35ceb3f1b50468b02bf2078e663839f217ef3c
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2022-05-27 12:02:14 -0500

    feat(auth): header btn

--- src/core/layout/Header.tsx
+++ src/core/layout/Header.tsx
@@ -1,22 +1,30 @@
 import { Link } from 'react-router-dom';
 
-export const Header = () => (
-  <header className="header shadow-lg">
-    <div className="m-auto flex h-full max-w-screen-lg flex-col items-center justify-center md:flex-row md:justify-between">
-      <Link to="/">
-        <h1 className=" text-xl font-bold text-primary-100">Recolectores</h1>
-      </Link>
-      <nav className="flex flex-row gap-3">
-        <Link className="btn btn--secondary" to="/create">
-          Registrar un Punto
+export const Header = () => {
+  const isLoged = false;
+  return (
+    <header className="header shadow-lg">
+      <div className="m-auto flex h-full max-w-screen-lg flex-col items-center justify-center md:flex-row md:justify-between">
+        <Link to="/">
+          <h1 className=" text-xl font-bold text-primary-100">Recolectores</h1>
         </Link>
-        <Link className="btn btn--primary" to="/login">
-          Login
-        </Link>
-        <button type="button" className="btn btn--tertiary">
-          Salir
-        </button>
-      </nav>
-    </div>
-  </header>
-);
+        <nav className="flex flex-row gap-3">
+          {isLoged ? (
+            <>
+              <Link className="btn btn--tertiary" to="/create">
+                Registrar un Punto
+              </Link>
+              <button type="button" className="btn btn--red">
+                Salir
+              </button>
+            </>
+          ) : (
+            <Link className="btn btn--tertiary" to="/login">
+              Login
+            </Link>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
